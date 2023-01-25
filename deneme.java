73fa3ffdcd7781f0538fc5d2266991b04672ee11
Huseyin-Gunaydin <gunaydinhuseyin1453@gmail.com>
2023-01-25 15:12:41 +0100

    on on

--- deneme.java
+++ deneme.java
@@ -1,6 +1,6 @@
 public class deneme {
     public static void main(String[] args) {
-        System.out.println("Hüseyin Günaydin");
+        System.out.println("Hüseyin Günaydin on  on ");
 
     }
 }
